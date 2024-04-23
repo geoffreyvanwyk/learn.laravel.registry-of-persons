d3d23b62334c8fbd616727c5126afa717876496a
Geoffrey Bernardo van Wyk <geoffrey@vanwyk.biz>
2024-04-23 11:20:09 +0200

    refactor(Person): rearrange use statements

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-use App\Casts\AsSouthAfricanId;
+use InvalidArgumentException;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
-use InvalidArgumentException;
+
+use App\Casts\AsSouthAfricanId;
 
 class Person extends Model
 {
